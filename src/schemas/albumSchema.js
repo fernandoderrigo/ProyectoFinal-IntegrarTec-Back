1d0769f3f0373c74b99f697b18219288e71a3d0a
SebastianGaleano20 <sebascarp9@gmail.com>
2024-09-17 00:10:07 -0300

    #202409091530

--- src/schemas/albumSchema.js
+++ src/schemas/albumSchema.js
@@ -2,6 +2,6 @@ import Joi from 'joi';
 
 export const createAlbumSchema = Joi.object({
     name: Joi.string().min(3).max(100).required(),
-    release_Date: Joi.date().required(),
+    release_Date: Joi.date().iso().required(),
     image_Url: Joi.string().optional()
 });
