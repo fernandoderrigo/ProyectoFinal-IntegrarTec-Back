97dc80739f70c456112b5e9274fd2a155e3accbe
sebastiangaleano.dev <sebastiangaleano.dev@gmail.com>
2024-09-18 14:02:25 +0000

    #2409061901

--- src/schemas/playlistSchema.js
+++ src/schemas/playlistSchema.js
@@ -1,19 +1,15 @@
 import Joi from 'joi';
 
-export const createPlaylist = Joi.object({
+export const createPlaylistSchema = Joi.object({
     name: Joi.string().required(),
     id_usuario: Joi.number().integer().required()
 });
 
-export const updateSchema = Joi.object({
+export const updatePlaylistSchema = Joi.object({
     name: Joi.string().required(),
     id_usuario: Joi.number().integer().required()
 });
 
 export const namePlaylistSchema = Joi.object({
     name: Joi.string().required(),
-});
-
-export const idPlaylistSchema = Joi.object({
-    id: Joi.number().integer().required(),
 });
