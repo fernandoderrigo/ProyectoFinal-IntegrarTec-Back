e47ee18aa4bb1424447a12427ebe3a895874fa44
sebastiangaleano.dev <sebastiangaleano.dev@gmail.com>
2024-09-19 14:34:49 +0000

    albumRouter configurated

--- src/routes/albumRoutes.js
+++ src/routes/albumRoutes.js
@@ -1,13 +1,16 @@
 import { Router } from "express";
-import { schemaValidator } from "../middlewares/schemaValidator.js";
 import albumController from "../controllers/albumController.js";
-import { createAlbumSchema } from "../schemas/albumSchema.js";
 
 const router = Router();
-const { createAlbum, getAllAlbums} = albumController();
+const { createAlbum, getAllAlbums, updateAlbum, deleteAlbum, getAlbumById} = albumController();
 
 router.route('/')
     .get(getAllAlbums)
     .post(createAlbum)
+
+router.route('/:id')
+    .get(getAlbumById)
+    .delete(deleteAlbum)
+    .put(updateAlbum)
 
 export default router
