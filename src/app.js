172ac86edeb558866bdec8dfbb68dc03c5bab51b
sebastiangaleano.dev <sebastiangaleano.dev@gmail.com>
2024-09-22 13:05:40 +0000

    index route fix

--- src/app.js
+++ src/app.js
@@ -13,7 +13,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/', (req, res) => {
+app.get('/', (_req, res) => {
     const responseServer = {
         status: 200,
         app: "Escucha Facil",
